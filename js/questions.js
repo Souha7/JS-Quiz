251251c9b48ffc3ce05bd610b08041e4244a053b
Souha <souha.habib7196@gmail.com>
2022-02-14 01:14:06 +0100

    First commit

---
+++ js/questions.js
@@ -0,0 +1,123 @@
+// Creating an array and passing the number, questions, options, and answers
+let questions = [
+
+    {
+        numb: 1,
+        question: "Which of the following is a server-side Java Script object?",
+        answer: "File",
+        options: [
+        "File",
+        "Functions",
+        "FileUpload",
+        "Date"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "IsNaN() Evaluates And Argument To Determine if Given Value:",
+        answer: "Is Not a Number",
+        options: [
+            "Is Not a Null",
+            "Is Not a Number",
+            "Is Not a New Object",
+            "None Of The Above"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Function is Used To Parse a String To Int:",
+        answer: "Int.Parse",
+        options: [
+            "Integer.Parse",
+            "Int.Parse",
+            "Parse.Int",
+            "None Of The Above"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Method Prompt() Contain ........Number of Parameters.",
+        answer: "Two",
+        options: [
+            "One",
+            "Two",
+            "Three",
+            "Zerp"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Event is Used To Check An Empty Text Box:",
+        answer: "OnBlur()",
+        options: [
+            "OnClick()",
+            "OnFocus()",
+            "OnBlur()",
+            "None"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "GetMonth() returns The Month as:",
+        answer: "Int",
+        options: [
+            "Int",
+            "Char",
+            "Float",
+            "String"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
+        answer: "Prompt()",
+        options: [
+            "Alert()",
+            "Prompt()",
+            "Confirm()",
+            "Msg()"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: " ___________ JavaScript is also called client-side JavaScript",
+        answer: "Navigator ",
+        options: [
+            "Microsoft",
+            "Navigator ",
+            "LiveWire",
+            "Native"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "How does Java Script store dates in objects of Date type?",
+        answer: "The number of milliseconds since January 1st, 1970",
+        options: [
+            "The number of days since January 1st, 1900",
+            "The number of seconds since January 1st, 1970",
+            "The number of picoseconds since January 1st, 1970",
+            "The number of milliseconds since January 1st, 1970"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "What is the alternate name for Java script?",
+        answer: "ECMAScript ",
+        options: [
+            "Pathname",
+            "LimeScript",
+            "ECMAScript ",
+            "None Of The Above "
+        ]
+    },
+];
